4c4a7c9ee4c587365f76f5c94d2e852a8e96977c
Nat <nat@cmmc.io>
2017-08-22 13:27:59 +0700

    - download latest release - ssl support - change webserver default port to 8080

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 #!/bin/ash
-darkhttpd /home/www
+
+wget $(wget -q -O- https://api.github.com/repos/cmmakerclub/cmmc-devices2/releases/latest | jq -r  ".assets[].browser_download_url") -O- | tar xvz -C /home/www
+# unzip -qqo dist.zip -d /home/www
+# tar -xvf dist.tar.gz -C /home/www
+
+darkhttpd /home/www --port 8080
