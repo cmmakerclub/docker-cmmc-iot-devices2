97887dc5e23afd9a95a47b5609f4e102cd027f29
Nat <nat@cmmc.io>
2017-08-22 13:59:33 +0700

    - add ssl support for wget - download cmmc-devices application

--- build_all.sh
+++ build_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -f Dockerfile.alpine -t cmmc/static-webserver:latest  .
-docker build -f Dockerfile.rpi -t cmmc/static-webserver:rpi .
-docker build -f Dockerfile.rpi2 -t cmmc/static-webserver:rpi2 .
-docker build -f Dockerfile.rpi3 -t cmmc/static-webserver:rpi3 .
+docker build -f Dockerfile.alpine -t cmmc/iot-cmmc-devices:latest  .
+docker build -f Dockerfile.rpi -t cmmc/iot-cmmc-devices:rpi .
+docker build -f Dockerfile.rpi2 -t cmmc/iot-cmmc-devices:rpi2 .
+docker build -f Dockerfile.rpi3 -t cmmc/iot-cmmc-devices:rpi3 .
--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/ash
 
-# wget $(wget -q -O- https://api.github.com/repos/cmmakerclub/cmmc-devices2/releases/latest | jq -r  ".assets[].browser_download_url") -O- | tar xvz -C /home/www
-# unzip -qqo dist.zip -d /home/www
-# tar -xvf dist.tar.gz -C /home/www
+wget $(wget -q -O- https://api.github.com/repos/cmmakerclub/cmmc-devices2/releases/latest | jq -r  ".assets[].browser_download_url") -O- | tar xvz -C /home/www
 
 darkhttpd /home/www --port 8080
