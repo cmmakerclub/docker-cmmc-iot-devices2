b400e96f78c0760676f9f828104d90e24d731db4
Nat <nat@cmmc.io>
2017-08-22 13:44:28 +0700

    - support raspberry pi - alpine

--- build_all.sh
+++ build_all.sh
@@ -1,2 +1,6 @@
-docker build -f Dockerfile-rpi -t cmmakerclub/static-webserver:rpi  .
-docker build -f Dockerfile -t cmmakerclub/static-webserver  .
+#!/bin/bash
+
+docker build -f Dockerfile.alpine -t cmmc/static-webserver:latest  .
+docker build -f Dockerfile.rpi -t cmmc/static-webserver:rpi .
+docker build -f Dockerfile.rpi2 -t cmmc/static-webserver:rpi2 .
+docker build -f Dockerfile.rpi3 -t cmmc/static-webserver:rpi3 .
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/ash
 
-wget $(wget -q -O- https://api.github.com/repos/cmmakerclub/cmmc-devices2/releases/latest | jq -r  ".assets[].browser_download_url") -O- | tar xvz -C /home/www
+# wget $(wget -q -O- https://api.github.com/repos/cmmakerclub/cmmc-devices2/releases/latest | jq -r  ".assets[].browser_download_url") -O- | tar xvz -C /home/www
 # unzip -qqo dist.zip -d /home/www
 # tar -xvf dist.tar.gz -C /home/www
 
