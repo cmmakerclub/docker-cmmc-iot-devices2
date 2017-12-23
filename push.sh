73dad8ea386951a1b80956474198d07d8bf789c3
Nat <nat@cmmc.io>
2017-12-23 13:22:35 +0700

    add push script.

---
+++ push.sh
@@ -0,0 +1,9 @@
+docker push cmmc/iot-cmmc-devices2:latest
+docker push cmmc/iot-cmmc-devices2:rpi
+docker push cmmc/iot-cmmc-devices2:rpi2
+docker push cmmc/iot-cmmc-devices2:rpi3
+
+docker push cmmc/iot-cmmc-devices:latest
+docker push cmmc/iot-cmmc-devices:rpi
+docker push cmmc/iot-cmmc-devices:rpi2
+docker push cmmc/iot-cmmc-devices:rpi3
