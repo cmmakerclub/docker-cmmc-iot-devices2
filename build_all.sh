72870350e15d9b0918a1eade799d18782ddb8688
Nat <nat@cmmc.io>
2017-12-23 13:04:43 +0700

    can override default mqtt user

--- build_all.sh
+++ build_all.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-docker build -f Dockerfile.alpine -t cmmc/iot-cmmc-devices:latest  .
-docker build -f Dockerfile.rpi -t cmmc/iot-cmmc-devices:rpi .
-docker build -f Dockerfile.rpi2 -t cmmc/iot-cmmc-devices:rpi2 .
-docker build -f Dockerfile.rpi3 -t cmmc/iot-cmmc-devices:rpi3 .
+docker build -f Dockerfile.alpine -t cmmc/iot-cmmc-devices2:latest  .
+docker build -f Dockerfile.rpi -t cmmc/iot-cmmc-devices2:rpi .
+docker build -f Dockerfile.rpi2 -t cmmc/iot-cmmc-devices2:rpi2 .
+docker build -f Dockerfile.rpi3 -t cmmc/iot-cmmc-devices2:rpi3 .
+
+docker tag cmmc/iot-cmmc-devices2:latest cmmc/iot-cmmc-devices:latest
+docker tag cmmc/iot-cmmc-devices2:rpi cmmc/iot-cmmc-devices:rpi
+docker tag cmmc/iot-cmmc-devices2:rpi2 cmmc/iot-cmmc-devices:rpi2
+docker tag cmmc/iot-cmmc-devices2:rpi3 cmmc/iot-cmmc-devices:rpi3
+
+#
+#docker push cmmc/iot-cmmc-devices2:latest
+#docker push cmmc/iot-cmmc-devices2:rpi
+#docker push cmmc/iot-cmmc-devices2:rpi2
+#docker push cmmc/iot-cmmc-devices2:rpi3
+#
+#docker push cmmc/iot-cmmc-devices:latest
+#docker push cmmc/iot-cmmc-devices:rpi
+#docker push cmmc/iot-cmmc-devices:rpi2
+#docker push cmmc/iot-cmmc-devices:rpi3
+#
--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #!/bin/ash
 
+env 
+echo $MQTT_HOST
 wget $(wget -q -O- https://api.github.com/repos/cmmakerclub/cmmc-devices2/releases/latest | jq -r  ".assets[].browser_download_url") -O- | tar xvz -C /home/www
+find /home/www
+sed -i "s/hostname: DEFAULT_MQTT_HOSTNAME/hostname: '$MQTT_HOST'/g" /home/www/main.bundle.js
+sed -i "s/port: DEFAULT_MQTT_WS_PORT/port: '$MQTT_WS_PORT'/g" /home/www/main.bundle.js
+sed -i "s/filter: DEFAULT_MQTT_FILTER/filter: '$MQTT_TOPIC_FILTER'/g" /home/www/main.bundle.js
+
+  # path: DEFAULT_MQTT_WS_PATH,
+  # filter: DEFAULT_MQTT_FILTER,
+  # topic: DEFAULT_MQTT_TOPIC
 
 darkhttpd /home/www --port 8080
